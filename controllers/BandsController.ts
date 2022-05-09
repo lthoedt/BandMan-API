e930d04eb82355b92fbd20ed3015d769d3e4e7c5
lthoedt <lthoedt@gmail.com>
2022-05-09 10:35:49 +0200

    added null safety to createBand

--- controllers/BandsController.ts
+++ controllers/BandsController.ts
@@ -1,21 +1,27 @@
-import {BandDTO} from '../dtos/BandDTO';
+import { BandDTO } from '../dtos/BandDTO';
 import express from 'express';
 import { createBand } from '../services/BandsService';
 import { Band } from '../database/nodes/Band';
+import { sendStatus } from './functions';
 
 const router = express.Router();
 
 // getBands
 router.get('/', async (req, res) => {
-    res.json({henk: true})
+    res.json({ henk: true })
 })
 
 // createBand
 router.post('/', async (req, res) => {
     const bandDto = BandDTO.fromJSON(req.body);
+    // TODO: find a away to automate this.
+    if (bandDto == null) {
+        sendStatus(res, 412, "");
+    }
+
     const band = Band.fromDTO(bandDto);
     const status = await createBand(band);
-    
+
     res.json({
         success: status
     })
