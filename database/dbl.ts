02d3e7d34aa4672f52c0074552ad1e92d17028a3
lthoedt <lthoedt@gmail.com>
2022-05-09 10:36:10 +0200

    Changed DBL name

--- database/dbl.ts
+++ database/dbl.ts
@@ -2,7 +2,7 @@ import neo4j from 'neo4j-driver'
 
 export const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('MusicMan', "Password123!"))
 export const session = driver.session({
-  database: 'MusicMan'
+  database: 'BandMan'
 })
 
 
